5a807f8bd94106ab1120f4c4ff59dfa43d6de629
Russel Zindoga <russellzindoga@gmail.com>
2024-07-11 20:03:45 +0200

    Create simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# define variables
+principal = 1000 
+rate = 0.05
+time = 3
+
+# formula for simple interest
+interest = principal * rate * time
+
+# result
+print(f"the simple interest is: {interest}")
